1889feaaa11b3befdeef8b0884fd92432e94874e
Fenomena"git config --global user.email Lizard-90@mail.rugit config --global user.name 'Lizard-90 <Lizard-90@mail.ru>
2023-04-18 02:10:09 +0300

    error fixed

--- app/src/main/java/com/example/booklibrary/CleanArchitectureBlueprintsApplication.kt
+++ app/src/main/java/com/example/booklibrary/CleanArchitectureBlueprintsApplication.kt
@@ -7,30 +7,31 @@ import com.example.booklibrary.mappers.BookWithStatusMapper
 import com.example.data.DB.repasitories.books.BooksRepositoryImp
 import com.example.domain.repositories.BooksRepository
 import com.example.domain.usecases.BookmarkBookUseCase
+import com.example.domain.usecases.GetBookmarkUseCase
 import com.example.domain.usecases.GetBooksUseCase
 import com.example.domain.usecases.UnbookmarkUseCase
 import timber.log.Timber
 
-class CleanAcritectureBluePrintsApplication : Application() {
-    private val booksRepository: BooksRepository
+class CleanArchitectureBlueprintsApplication : Application() {
+    private val booksRepository: BooksRepositoryImp
         get() = ServiceLocator.provideBooksRepository(this)
 
-    val getBookUseCase: GetBooksUseCase
+    val getBooksUseCase: GetBooksUseCase
         get() = GetBooksUseCase(booksRepository)
 
-    val getBoolmarksUseCase: GetBooksUseCase
-        get() = GetBooksUseCase(booksRepository)
+    val getBookmarksUseCase: GetBookmarkUseCase
+        get() = GetBookmarkUseCase(booksRepository)
 
     val bookmarkBooksUseCase: BookmarkBookUseCase
         get() = BookmarkBookUseCase(booksRepository)
 
-    val unbookmarkBooksUseCase: UnbookmarkUseCase
+    val unbookmarkBookUseCase: UnbookmarkUseCase
         get() = UnbookmarkUseCase(booksRepository)
 
     val bookWithStatusMapper = BookWithStatusMapper()
 
-    fun onCreate(saveInstanceState: Bundle) {
+    override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree());
+        Timber.plant(Timber.DebugTree())
     }
 }
